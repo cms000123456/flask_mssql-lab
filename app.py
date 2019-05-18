9bf66092ef1f50d637af892fd4d439f1b9e6c66c
cms000123456 <36997018+cms000123456@users.noreply.github.com>
2019-05-18 09:32:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,29 +7,41 @@ import pyodbc
 
 app = Flask(__name__)
 
-connection = pyodbc.connect('Driver={SQL Server};Server=.;Database=testdb01;Trusted_Connection=yes')
-cursor = connection.cursor()  
-cursor.execute("SELECT * FROM testtable")
-column_names = [d[0] for d in cursor.description] 
-s = "<table id='tab01'><tr>"
-for col in column_names:
-    s = s + "<th>" + col + "</th>"
-s = s + "</tr>"  
-for row in cursor:  
-    s = s + "<tr>"  
-    for x in row:  
-        s = s + "<td>" + str(x) + "</td>"  
-s = s + "</tr>"  
+def listdb():
+    connection = pyodbc.connect('Driver={SQL Server};Server=.;Database=testdb01;Trusted_Connection=yes')
+    cursor = connection.cursor()  
+    cursor.execute("SELECT * FROM testtable")
+    column_names = [d[0] for d in cursor.description] 
+    s = "<table id='tab01'><tr>"
+    for col in column_names:
+        s = s + "<th>" + col + "</th>"
+    s = s + "</tr>"  
+    for row in cursor:  
+        s = s + "<tr>"  
+        for x in row:  
+            s = s + "<td>" + str(x) + "</td>"  
+    s = s + "</tr>"
+    connection.close()
+    return str("<html>" + css + "<body>" + s + "</body></html>")
 
+def inserttodb(name,number):
+    connection = pyodbc.connect('Driver={SQL Server};Server=.;Database=testdb01;Trusted_Connection=yes')
+    cursor = connection.cursor()  
+    cursor.execute("insert into testtable(name, number) values (?, ?)", name, number)
+    connection.commit()	
+    connection.close()
+    return (name,number)	
+	
+	
 css = """<head><style>
 #tab01 {border-collapse: collapse;width: 100%;}
 #tab01 td, #tab01 th {border: 1px solid #ddd;padding: 8px;}
-#tab01 tr:nth-child(even){background-color: #f2f2f2;}
+#tab01 tr:nth-child(odd){background-color: #f2f2f2;}
 #tab01 tr:hover {background-color: #ddd;}
-#tab01 th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: #4CAF50;color: white;}
+#tab01 th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: #d580ff;color: white;}
 </style></head>"""
 
-connection.close()  
+#connection.close()  
 
 @app.route('/')
 def wanip():
@@ -38,8 +50,23 @@ def wanip():
 
 @app.route('/db01')
 def db01():
-    return "<html>" + css + "<body>" + s + "</body></html>"
-    #return jsonify(column_names)
+    list = listdb()
+    return list
+
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
+    if request.method == 'GET':
+        out = listdb()
+        out = out + render_template('insert.html')
+        return out
+        #return render_template('insert.html')
+    if request.method == 'POST':		
+        result = inserttodb(name=request.form["name"],number=request.form["number"])
+        out = listdb()
+        out = out + render_template('insert.html')
+        return out
+        #return render_template('insert.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,debug=True)
